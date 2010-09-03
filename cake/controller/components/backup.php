ba8d56d13efc1e8b886d74ed85a4550518959a50
Silas Ribas <silasrm@silas-laptop.(none)>
2010-09-03 16:33:34 -0300

    Adicionado o licenciamento via GPL v3.

--- cake/controller/components/backup.php
+++ cake/controller/components/backup.php
@@ -1,5 +1,24 @@
 <?php
 
+    /*
+        Classe BackupComponent. Classe que cria um component para CakePHP que integra o Backup-PHP com o CakePHP,
+        ficando disponível para ser usado nos controllers.
+        Copyright (C) 2010  Silas Ribas Martins and Hugo Henrique
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    */
+
     set_include_path( APP . 'vendors' . PATH_SEPARATOR . get_include_path() );
 
     App::import( 'Vendor', 'Backup', array( 'file' => 'backup/Backup.php' ) );
